fc9839933aeb215f27168fc27a99d14561f8df58
MarcondesJuniorDev <mncjdev@gmail.com>
2025-07-23 10:43:54 -0400

    Adiciona título e slug à classe Educational

--- app/Filament/Clusters/Educational.php
+++ app/Filament/Clusters/Educational.php
@@ -8,4 +8,6 @@ class Educational extends Cluster
 {
     protected static ?string $navigationIcon = 'heroicon-s-academic-cap';
     protected static ?string $activeNavigationIcon = 'heroicon-o-academic-cap';
+    protected static ?string $title = 'Educacional';
+    protected static ?string $slug = 'educacional';
 }
