bf7b73135450174b8f8b4b2b7c04e1a1ad49593a
MarcondesJuniorDev <mncjdev@gmail.com>
2025-07-23 10:33:31 -0400

    Adiciona a classe Educational com ícones de navegação

---
+++ app/Filament/Clusters/Educational.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class Educational extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-s-academic-cap';
+    protected static ?string $activeNavigationIcon = 'heroicon-o-academic-cap';
+}
