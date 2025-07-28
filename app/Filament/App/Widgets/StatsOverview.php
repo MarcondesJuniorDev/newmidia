cebd48805833d894a3bcd90d303910826392c86a
MarcondesJuniorDev <mncjdev@gmail.com>
2025-07-28 09:53:29 -0400

    Adiciona widget StatsOverview para exibir estatísticas de anexos

---
+++ app/Filament/App/Widgets/StatsOverview.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\App\Widgets;
+
+use App\Models\User;
+use App\Models\Content;
+use Filament\Support\Enums\IconPosition;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            // Anexos
+            Stat::make('Total Attachments', User::class)
+                ->label('Anexos')
+                ->url('/admin/anexos')
+                ->description("Total de anexos cadastrados no sistema")
+                ->descriptionIcon('heroicon-o-paper-clip', IconPosition::Before)
+                ->chart(
+                    array_map(
+                        fn($x) => pow($x, 2),
+                        range(1, 6)
+                    )
+                )
+                ->value(fn() => Content::count())
+                ->color('primary'),
+        ];
+    }
+}
