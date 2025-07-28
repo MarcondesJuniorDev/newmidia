e6c1931427aced3bd55ca7a03d6760fbd974dbb2
MarcondesJuniorDev <mncjdev@gmail.com>
2025-07-28 09:53:41 -0400

    Remove widgets do painel e ajusta descoberta de widgets no AppPanelProvider

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -36,14 +36,12 @@ class AppPanelProvider extends PanelProvider
             ->maxContentWidth(MaxWidth::Full)
             ->discoverResources(in: app_path('Filament/App/Resources'), for: 'App\\Filament\\App\\Resources')
             ->discoverPages(in: app_path('Filament/App/Pages'), for: 'App\\Filament\\App\\Pages')
+            ->discoverWidgets(in: app_path('Filament/App/Widgets'), for: 'App\\Filament\\App\\Widgets')
             ->pages([
                 Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/App/Widgets'), for: 'App\\Filament\\App\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-            ])
+            ->widgets([])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
